d958b3376b582db9b4da67a7f34d2ccd409fdec1
koheitakahashi <426koohei@gmail.com>
2023-05-15 18:43:20 +0900

    div タグの下に任意のテキストを挿入できるようにしてみた

--- app.rb
+++ app.rb
@@ -16,10 +16,14 @@ end
 content_script site: "www.example.com" do
   h1 = document.querySelector("h1")
   h1.innerText = "Hello unloosen!"
+  existed_div = document.querySelector("div")
+  p_element = document.createElement('p')
 
   chrome.runtime.onMessage.addListener do |message|
-    h1 = document.querySelector("h1")
-    h1.innerText = message
+    new_text_element = document.createTextNode(message)
+    p_element.appendChild(new_text_element)
+    existed_div.appendChild(p_element)
+
     true
   end
 end
@@ -27,9 +31,8 @@ end
 popup do
   submit_button = document.querySelector("button#submit")
   submit_button.addEventListener "click" do |e|
-    form_text = document.querySelector("input#title").value
+    form_text = document.querySelector("input#input").value
 
-    # This is workaround: in popup, JS.eval cannot be used.
     query_object = Utils.build_js_object(active: true, currentWindow: true)
     chrome.tabs.query(query_object) do |tabs|
       tab = tabs.at(0)
